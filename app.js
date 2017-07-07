ae8af2a1b30e2d891f00ab8bf30aab843000453d
Errindel <adriana.olszak@gmail.com>
2017-07-07 23:46:28 +0200

    setting up the environment

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+'use strict';
 const express = require('express');
-
 
 const app = express();
 
-app.listen(port, (err)=>{
+const port = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+app.set('views', './src/views');
+
+const handlebars = express('express-handlebars');
+app.engine('.hbs', handlebars({extname: '.hbs'}));
+
+app.set('view engine', '.hbs');
+
+app.get('/', (req, res) => {
+
+});
+
+app.get('/books', (req, res) => {
+    res.send('Hello books');
+});
+
+app.listen(port, (err) => {
 
 });
